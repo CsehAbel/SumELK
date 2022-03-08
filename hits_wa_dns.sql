8b15368119b23d5a048da1a9edebdd990a5d0290
abel.kecse <abel.kecse@4p-service.de>
2022-03-08 11:04:43 +0100

    resolveIpToName

---
+++ hits_wa_dns.sql
@@ -0,0 +1,11 @@
+SELECT * FROM white_apps_dns WHERE dns is NULL LIMIT 30000;
+SELECT COUNT(*) FROM white_apps_dns;
+
+SELECT COUNT(*) FROM ip_unique;
+SELECT * FROM ip_unique;
+SELECT src_ip FROM ip_unique GROUP BY src_ip LIMIT 20000;
+SELECT COUNT(*) FROM (SELECT * FROM ip_unique GROUP BY src_ip) as s;
+
+SELECT * FROM src_dns WHERE dns is NULL LIMIT 30000;
+SELECT * FROM src_dns WHERE dns is NOT NULL LIMIT 30000;
+SELECT COUNT(*) FROM white_apps_dns;
--- resolveIpToName.py
+++ resolveIpToName.py
@@ -12,9 +12,7 @@ import dns.resolver, dns.reversename
 def ip2dns(ip): #defthw99m5bsrv.ad001.siemens.net 139.23.160.99
 
     resolver = dns.resolver.Resolver()
-    resolver.nameservers = ['129.103.90.199',
-                            '129.103.99.237',
-                            '139.25.226.6']
+    resolver.nameservers = ["ad101.siemens-energy.net", "ad001.Siemens.net", "139.25.226.62"]
     #ip='139.21.146.17'
     n = dns.reversename.from_address(ip)
     try:
@@ -49,13 +47,12 @@ def main():
     sqlEngine = create_engine(
         'mysql+pymysql://%s:%s@%s/%s' % (secrets.mysql_u, secrets.mysql_pw, "127.0.0.1", "CSV_DB"), pool_recycle=3600)
     dbConnection = sqlEngine.connect()
-    wa_ips=pd.read_sql_query("SELECT IPs FROM white_apps_se_ruleset",dbConnection)
-    wa_ips['dns'] = wa_ips["IPs"].map(lambda a: ip2dns(a))
+    wa_ips = pd.read_sql_query("SELECT src_ip FROM ip_unique GROUP BY src_ip", dbConnection)
+    wa_ips['dns'] = wa_ips["src_ip"].map(lambda a: ip2dns(a))
     sqlEngine = create_engine(
         'mysql+pymysql://%s:%s@%s/%s' % (secrets.mysql_u, secrets.mysql_pw, "127.0.0.1", "CSV_DB"), pool_recycle=3600)
     dbConnection = sqlEngine.connect()
-    wa_ips.to_sql("white_apps_dns", dbConnection, if_exists='replace', index=True)
-
+    wa_ips.to_sql("src_dns", dbConnection, if_exists='replace', index=True)
     # print("")
 
 
