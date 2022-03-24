8aa1626e6c1ca055103a3d5581486ce35db42fe5
abel.kecse <abel.kecse@4p-service.de>
2022-03-24 12:32:53 +0100

    application.py changed

--- application.py
+++ application.py
@@ -1,3 +1,5 @@
+import json
+
 import resolveIpToName
 import systems_group
 import qc_to_sql
@@ -11,19 +13,27 @@ def main():
     #downloads SecureTrack
     # where rule_name like a.* and like wuser.*  and not like atos_vuln_scan
     # to CSV_DB -> st_ports
-    systems_group.dest_ports_to_file()
+    #systems_group.dest_ports_to_file()
     #first run SGRE to unpack se_ruleset
-    filepath_qc = "se_ruleset_unpacked22Mar2022.xlsx"
-    qc_to_sql.main(filepath_qc)
+    #filepath_qc = "se_ruleset_unpacked22Mar2022.xlsx"
+    #qc_to_sql.main(filepath_qc)
 
     #download hits to hits/...json
-    hits.main()
+    #hits.main()
     # /mnt/c/ProgramData/MySQL/MySQL Server 8.0/Uploads
 
 
     #(onlyInOld,onlyInNew)
-    (onlyInOld,onlyInNex)=systems_group.all_red_networks_systems()
-    onlyInOld
+    (onlyInOld,onlyInNew)=systems_group.all_red_networks_systems()
+
+    with open('transform.json') as json_file:
+        transform = json.load(json_file)
+    transform['bool']['filter']['terms']['source.ip']=onlyInNew
+
+    with open('new_transform.json', 'w') as outfile:
+       json.dump(transform, outfile)
+
+    print("")
     #resolving ip to fqdn for white_apps
     #each time the ip-fqdn pair will be appended to CSV_DB->src_dns
     # sqlEngine = create_engine(
