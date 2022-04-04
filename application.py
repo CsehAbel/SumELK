f8a40da4eedd0c01f28b1f82b379d6ba93ea5cdc
abel.kecse <abel.kecse@4p-service.de>
2022-04-04 17:11:44 +0200

    added cli argument for exploded seruleset

--- application.py
+++ application.py
@@ -11,6 +11,9 @@ from sqlalchemy import create_engine
 
 import secrets
 import pandas
+import shlex
+import sys
+import argparse
 
 def save_new_transform_json():
 
@@ -33,6 +36,16 @@ def save_new_transform_json():
 
     print("Done!")
 
+def get_cli_args():
+    parser = argparse.ArgumentParser("Unpacking Quality Check xlsx")
+    parser.add_argument(
+        '--qualitycheck', dest="qualitycheck", type=str, required=True,
+        help="Path of QualityCheck.xlsx"
+    )
+    #ToDo add command line argument for excel file
+    args = parser.parse_args(shlex.split(" ".join(sys.argv[1:])))
+    return args
+
 def main():
     # downloads SecureTrack
     # where rule_name like a.* and like wuser.*  and not like atos_vuln_scan
@@ -40,19 +53,19 @@ def main():
     # systems_group.dest_ports_to_file()
     # first run SGRE to unpack se_ruleset
     # filepath_qc = "se_ruleset_unpacked22Mar2022.xlsx"
-    # qc_to_sql.main(filepath_qc)
+    #filepath_qc = get_cli_args().qualitycheck
+    #qc_to_sql.main(filepath_qc)
+    #downloading securetrack dest_ip,port
+    systems_group.dest_ports_to_file()
 
     # download hits to hits/...json
     # hits.main()
     # /mnt/c/ProgramData/MySQL/MySQL Server 8.0/Uploads
 
-    save_new_transform_json()
+    #save_new_transform_json()
 
     # resolving ip to fqdn for white_apps
     # each time the ip-fqdn pair will be appended to CSV_DB->src_dns
-    # sqlEngine = create_engine(
-    #     'mysql+pymysql://%s:%s@%s/%s' % (secrets.mysql_u, secrets.mysql_pw, "127.0.0.1", "CSV_DB"), pool_recycle=3600)
-    # dbConnection = sqlEngine.connect()
     # resolveIpToName.resolve_white_apps(sqlEngine, dbConnection)
 
 if __name__ == "__main__":
