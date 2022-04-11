09c363fdf9f7062349a3a6f16c8dfadfd4fc97cc
abel.kecse <abel.kecse@4p-service.de>
2022-04-11 03:23:35 +0200

    modified query.json and download_index

--- main.py
+++ main.py
@@ -1,57 +1,13 @@
 #!/home/scripts/ticket_automatisierung/bin/python3
 import datetime
-import logging
-import argparse
-import math
-import shlex
-import sys
-import os
 
 import pandas
-from pytos.common.functions.config import Secure_Config_Parser
-from pytos.common.logging.definitions import COMMON_LOGGER_NAME
-from pytos.common.logging.logger import setup_loggers
-from pytos.securechange.helpers import Secure_Change_Helper
-from pytos.securechange.xml_objects.rest import Group_Change_Member_Object
-from pytos.securetrack.helpers import Secure_Track_Helper
-from pytos.common.definitions.xml_tags import Attributes
-from pytos.securechange.xml_objects.rest import Group_Change_Member_Object,Group_Change_Node,Step_Field_Multi_Group_Change
-from pytos.common.base_types import XML_List
-
 from elasticsearch import Elasticsearch
 import json
-from ssl import create_default_context
+
 from elasticsearch import RequestsHttpConnection
 
 import secrets
-import systems_group
-
-
-sc_helper = Secure_Change_Helper("cofw.siemens.com", (secrets.sc_u, secrets.sc_pw))
-st_helper = Secure_Track_Helper("cofw-track.siemens.com", (secrets.st_u,secrets.st_pw))
-
-log_file_path="/home/scripts/"
-log_file_name="pytos_logger_sumelk.log"
-config_file_path='pytos.conf'
-
-logger = logging.getLogger(COMMON_LOGGER_NAME)
-conf = Secure_Config_Parser(config_file_path=config_file_path)
-setup_loggers(conf.dict("log_levels"),log_file_path,log_file_name,log_to_stdout=True)  # cli_args.debug)
-
-# log_file2="/home/scripts/notfound.log"
-#
-# logger2 = logging.getLogger(__name__)
-# logger2.setLevel(logging.DEBUG)
-# formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
-# file_handler = logging.FileHandler(log_file2)
-# file_handler.setLevel(logging.INFO)
-# file_handler.setFormatter(formatter)
-# stream_handler = logging.StreamHandler()
-# stream_handler.setFormatter(formatter)
-# logger2.addHandler(file_handler)
-# logger2.addHandler(stream_handler)
-#
-# logger2.info("Script called.")
 
 user=secrets.sc_u
 pw=secrets.sc_pw
@@ -59,14 +15,6 @@ host='sn1hot03.ad001.siemens.net'
 port='9200'
 
 def main():
-
-    device_name = "CST-P-SAG-Energy"
-    #device_id = st_helper.get_device_id_by_name(device_name)
-    #rules=st_helper.get_rules_for_device(device_id)
-    #use aggregation dest_ip(source_ip), look at the results with pandas scientific viewer in debug mode
-    #(query-range) + (query-rule_name)
-    #GET /my-index-000001/_search?typed_keys The response returns the aggregation type as a prefix to the aggregation’s name.
-    #print matched iterating: lista["i_f","vuser","wuser123","a_123","App_123"]
 
     es = Elasticsearch([host], port=port, connection_class=RequestsHttpConnection,
                        http_auth=(user, pw), use_ssl=True, verify_certs=False, timeout=120, retry_on_timeout=True, max_retries=3)
@@ -80,57 +28,109 @@ def main():
     #lt_date = datetime.datetime(day=23, year=2022, month=1)
     dtn=datetime.datetime.now()
     lt_date = datetime.datetime(day=dtn.day, year=dtn.year, month=dtn.month)
-    duration1 = datetime.timedelta(days=90)
+    duration1 = datetime.timedelta(minutes=10)
     gte_date = lt_date - duration1
-
-    #lt_date = lt_date - duration1
-    #duration2 = datetime.timedelta(days=30)
-    #gte_date = lt_date - duration2
 
     gte_date = gte_date.strftime("%Y-%m-%dT%H:%M:%S")
     lt_date = lt_date.strftime("%Y-%m-%dT%H:%M:%S")
-    #query['bool']['filter'][1]['range']['@timestamp']['gte']=gte_date
-    #query['bool']['filter'][1]['range']['@timestamp']['lt']=lt_date
+    query["bool"]['filter']['range']['@timestamp']['gte']=gte_date
+    query["bool"]['filter']['range']['@timestamp']['lt']=lt_date
 
+    indices=["sfs-yellow-checkpoint-000001"
+    ,"siemens-yellow-checkpoint-000001"
+    ,"express-yellow-checkpoint-000001"
+    ,"cz-yellow-checkpoint-000001"
+    ,"energy-yellow-checkpoint-000001"
+    ]
 
-    for i in range(4):
-        download_index(es=es,index="business_partner_00%d" % (i+1),nth=(i+1),sort="_doc",gte_date=gte_date)
+    for i in range(indices.__len__()):
+        current_index=indices[i]
+        download_index(es=es,index=current_index,query=query,sort="_doc",gte_date=gte_date,fields=[
+          "source.ip",
+          "destination.ip",
+          "source.port",
+          "destination.port",
+          "rule.name",
+          "observer.ingress.interface.name",
+          "input.type"
+        ])
     
     print("Done!")
 
-def download_index(es,index,nth,sort,gte_date):
-    resp = es.search(index=index,sort=sort,size=10000)
+
+
+
+
+def download_index(es,index,sort,gte_date,query,fields):
+    resp = es.search(index=index,query=query,sort=sort,size=10000,fields=fields)
     #hits_len = resp['hits']['total']['value']
     hits_len = len(resp['hits']['hits'])
     print("Got %d Hits:" % hits_len)
     seq = 0
     hits = resp['hits']['hits']
-    with open('hits/hit_00%d_%s_%d.json' % (nth, gte_date, seq), 'w') as outfile:
-        # json.dump(buckets)
+    with open('yellow/%s_%s_%d.json' % (index, gte_date, seq), 'w') as outfile:
         for b in hits:
-            json.dump(flattenhit(b), outfile)
+            b=flattenhit(b)
+            json.dump(b, outfile)
             outfile.write("\n")
     seq = seq + 1
 
     while hits_len >= 10000:
         search_after=resp['hits']['hits'][-1]['sort']
-        resp = es.search(index=index,sort=sort,search_after=search_after,size=10000)
+        resp = es.search(index=index,search_after=search_after,query=query,sort=sort,size=10000,fields=fields)
         # hits_len = resp['hits']['total']['value']
         hits_len = len(resp['hits']['hits'])
         print("Got %d Hits:" % hits_len)
         hits = resp['hits']['hits']
 
-        with open('hits/hit_00%d_%s_%d.json' % (nth,gte_date,seq), 'w') as outfile:
+        with open('yellow/%s_%s_%d.json' % (index,gte_date,seq), 'w') as outfile:
             # json.dump(buckets)
             for b in hits:
-                json.dump(flattenhit(b), outfile)
+                b = flattenhit(b)
+                json.dump(b, outfile)
                 outfile.write("\n")
         seq = seq + 1
 
 def flattenhit(h):
-    s=h['_source']['source']['ip']
-    d=h['_source']['destination']['ip']
-    return {"source_ip":s,"dest_ip":d}
+            # "field": "source.port",
+            # "field": "destination.port",
+            # "field": "observer.ingress.interface.name",
+            # "field": "input.type",
+            # "field": "rule.name"
+        h=h['fields']
+        try:
+            s = h["source.ip"][0]
+        except KeyError as e:
+            print("")
+        d = h["destination.ip"][0]
+        if "source.port" in h:
+            sp = h["source.port"][0]
+        else:
+            sp = None
+        if "destination.port" in h:
+            dp = h["destination.port"][0]
+        else:
+            dp = None
+        if "input.type" in h:
+            p = h["input.type"][0]
+        else:
+            p = None
+        if "observer.ingress.interface.name" in d:
+            r = h["observer.ingress.interface.name"][0]
+        else:
+            r = None
+        if "rule.name" in h:
+            i = h["rule.name"][0]
+        else:
+            i = None
+        return {"source_ip": s, "dest_ip": d, "source_port": sp,
+                "dest_port": dp, "protocol": p, "rule_name": r, "interface": i
+                }
+
+
+def flattenbucket(h):
+    h=h['key']
+    return h
 
 if __name__ == '__main__':
     main()
