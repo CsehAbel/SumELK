088143f7563a9fb46f8f0c0886e1d2724acf20a5
abel.kecse <abel.kecse@4p-service.de>
2022-05-09 02:55:10 +0200

    systems_group.py importer ip_utils.py

--- systems_group.py
+++ systems_group.py
@@ -23,6 +23,7 @@ import json
 import secrets
 import bulk_json_to_df
 from sqlalchemy import create_engine
+import ip_utils
 
 
 sc_helper = Secure_Change_Helper("cofw.siemens.com", (secrets.sc_u, secrets.sc_pw))
@@ -36,141 +37,11 @@ logger = logging.getLogger(COMMON_LOGGER_NAME)
 conf = Secure_Config_Parser(config_file_path=config_file_path)
 setup_loggers(conf.dict("log_levels"),log_file_path,log_file_name,log_to_stdout=True)  # cli_args.debug)
 
-def netmask_to_cidr(netmask):
-    '''
-    :param netmask: netmask ip addr (eg: 255.255.255.0)
-    :return: equivalent cidr number to given netmask ip (eg: 24)
-    '''
-    return sum([bin(int(x)).count('1') for x in netmask.split('.')])
-
-def ip2int(addr):
-    return struct.unpack("!I", socket.inet_aton(addr))[0]
-
-def int2ip(addr):
-    return socket.inet_ntoa(struct.pack("!I", addr))
-
-
-
-def cidr_to_netmask(cidr):
-  cidr = int(cidr)
-  mask = (0xffffffff >> (32 - cidr)) << (32 - cidr) # wenn cidr=24, 32-cidr = 8
-  #0xffffffff >> 8 = int("0000 0000 1111 1111 1111 1111 1111 1111")
-  #0xffffff << 8 ->  int("0000 0000 1111 1111 1111 1111 1111 1111") << 8 = int("1111 1111 1111 1111 1111 1111 0000 0000")
-  #int("0000 0000 1111 1111 1111 1111 1111 1111") << 8 = ˜int("0000 0000 1111 1111 1111 1111 1111 1111")=int("1111 1111 1111 1111 1111 1111 0000 0000")
-  #~x
-  #Returns the complement of x - the number you get by switching each 1 for a 0 and each 0 for a 1
-  return integerToDecimalDottedQuad(mask)
-
-def netmask_to_cidr(netmask):
-    '''
-    :param netmask: netmask ip addr (eg: 255.255.255.0)
-    :return: equivalent cidr number to given netmask ip (eg: 24)
-    '''
-    return sum([bin(int(x)).count('1') for x in netmask.split('.')])
-
-def integerToDecimalDottedQuad(ip_int):
-  return (str( (0xff000000 & ip_int) >> 24)   + '.' +
-          str( (0x00ff0000 & ip_int) >> 16)   + '.' +
-          str( (0x0000ff00 & ip_int) >> 8)    + '.' +
-          str( (0x000000ff & ip_int)))
-
-def makeIntegerMask(cidr):
-    #return a mask of n bits as a long integer
-    mask = (0xffffffff >> (32 - cidr)) << (32 - cidr)
-    return mask
-
-def decimalDottedQuadToInteger(dottedquad):
-    #convert decimal dotted quad string to long integer"
-    #@ is native, ! is big-endian, native didnt work" \
-    #returned the octects reversed main.integerToDecimalDottedQuad(main.decimalDottedQuadToInteger('149.246.14.224'))"
-    ip_as_int = struct.unpack('!i', socket.inet_aton(dottedquad))[0]
-    if ip_as_int < 0:
-        ip_as_int=ip_as_int + 2**32
-    return ip_as_int
-
-def correctAndCheckMatchedMask(cidr):
-    patternMask = re.compile('[^\d]*(\d+)[^\d]*$')
-    resultMask = patternMask.match(cidr)
-    mask = resultMask.group(1)
-    mask = int(mask)
-    if mask >= 8 and mask <= 32:
-        return mask
-    else:
-        raise BaseException("Mask is less,equal to 16, mask is bigger,equal to 32")
-
-
-def isMask(cidr):
-    patternMask = re.compile('[^\d]*(\d+)[^\d]*$')
-    resultMask = patternMask.match(cidr)
-    if resultMask:
-        return True
-    else:
-        return False
-
-def old_decimalDottedQuadToInteger(dottedquad):
-    #convert decimal dotted quad string to long integer"
-    #@ is native, ! is big-endian, native didnt work" \
-    #returned the octects reversed main.integerToDecimalDottedQuad(main.decimalDottedQuadToInteger('149.246.14.224'))"
-    return struct.unpack('!i', socket.inet_aton(dottedquad))[0]
-
-
-def isIntegerAddressInIntegerNetwork(ip,net):
-   #Is an address in a network"
-   return ip & net == net
-
-def isPrefix(ipaddr):
-    patternPrefix = re.compile('.*?([0-9]{1,3}[^\d]+[0-9]{1,3}[^\d]+[0-9]{1,3}[^\d]+[0-9]{1,3}).*$')
-    resultPrefix = patternPrefix.match(ipaddr)
-    #first digit that it starts with is [1-9]
-    patternPrefixCommaSeparated = re.compile('[^\d]*?([1-9][0-9]{10,11}).*$')
-    resultPrefixCommaSeparated = patternPrefixCommaSeparated.match(ipaddr)
-    if resultPrefix or resultPrefixCommaSeparated:
-        return True
-    else:
-        return False
-
-def correctMatchedPrefix(ipaddr):
-
-    patternPrefixCommaSeparated = re.compile('^\s*([1-9][0-9]{10,11})\s*$')
-    resultPrefixCommaSeparated = patternPrefixCommaSeparated.match(ipaddr)
-
-    if resultPrefixCommaSeparated:
-        digits = [int(x) for x in str(resultPrefixCommaSeparated.group(1))]
-        l=len(digits)
-        fourthoctet  = [digits[l-3]*100, digits[l-2]*10, digits[l-1]]
-        thirdoctet = [digits[l-x]*math.pow(10, x-4)  for x in range(6, 3, -1)]
-        secondoctet  = [digits[l-x]*math.pow(10, x-7) for x in range(9, 6, -1)]
-        firstoctet = [digits[l-x]*math.pow(10, x-10) for x in range(l, 9, -1)]
-        ip = ".".join([str(int(sum(firstoctet))),str(int(sum(secondoctet))),str(int(sum(thirdoctet))),str(int(sum(fourthoctet)))])
-        return ip
-
-def ip_range_explode(ip,netmask):
-
-        prefix2 = ip
-        cidr2 = netmask_to_cidr(netmask)
-
-        base = integerToDecimalDottedQuad(
-            decimalDottedQuadToInteger(prefix2) & makeIntegerMask(
-                cidr2))
-        if base != prefix2:
-            print("Not a network Adresse (possible ip base %s)" % base)
-
-        int_prefix_top = (~makeIntegerMask(
-            cidr2)) | decimalDottedQuadToInteger(prefix2)
-        prefix_top = integerToDecimalDottedQuad(int_prefix_top)
-        print("netw.adrr.:{}".format(base))
-        list_unpacked_ips=[]
-        for j in range(decimalDottedQuadToInteger(base) + 1,
-                       decimalDottedQuadToInteger(
-                           integerToDecimalDottedQuad(int_prefix_top)) + 1):
-            list_unpacked_ips.append(integerToDecimalDottedQuad(j))
-        return list_unpacked_ips
-
 def append_to_list(not_g_no,input_list):
     if isinstance(not_g_no, Host_Network_Object):
         input_list.append(not_g_no.ip)
     else:
-        [input_list.append(sipa) for sipa in ip_range_explode(not_g_no.ip, not_g_no.netmask)]
+        [input_list.append(sipa) for sipa in ip_utils.ip_range_explode(not_g_no.ip, not_g_no.netmask)]
 
 def get_systems_ip_list():
     device_name = "CST-P-SAG-Energy"
@@ -189,7 +60,7 @@ def get_systems_ip_list():
         SystemsNetworkObjects = st_helper.get_network_objects_for_device(device_id, "group",{"name": gn })
         for i in SystemsNetworkObjects.network_objects:
             if(hasattr(i,"ip") and hasattr(i,"netmask")):
-                range={"ip":i.ip,"cidr":netmask_to_cidr(i.netmask)}
+                range={"ip":i.ip,"cidr":ip_utils.netmask_to_cidr(i.netmask)}
                 system_ips.append("%s/%s" %(range["ip"],range["cidr"]))
             else:
                 pass
@@ -256,8 +127,8 @@ def get_dest_ports():
                                         raise qex
                         elif aex.args[0]=='\'Range_Network_Object\' object has no attribute \'ip\'':
                             r_no=st_helper.get_network_object_by_device_and_object_id(device_id, x)
-                            for ra in range(ip2int(r_no.first_ip),ip2int(r_no.last_ip)+1):
-                                r_ip=int2ip(ra)
+                            for ra in range(ip_utils.ip2int(r_no.first_ip),ip_utils.ip2int(r_no.last_ip)+1):
+                                r_ip=ip_utils.int2ip(ra)
                                 l_dest_ip.append(r_ip)
 
                         else:
@@ -436,15 +307,15 @@ def onlyinold_to_sql():
 
             prefix2 = resultPrefix.group(2)
             cidr = resultPrefix.group(3)
-            cidr2 = correctAndCheckMatchedMask(cidr)
-            base = integerToDecimalDottedQuad(
-                decimalDottedQuadToInteger(prefix2) & makeIntegerMask(
+            cidr2 = ip_utils.correctAndCheckMatchedMask(cidr)
+            base = ip_utils.integerToDecimalDottedQuad(
+                ip_utils.decimalDottedQuadToInteger(prefix2) & ip_utils.makeIntegerMask(
                     cidr2))
             if base != prefix2:
                 print("Not a network Adresse (possible ip base %s)" % base)
 
-            int_prefix_top = (~makeIntegerMask(
-                cidr2)) | decimalDottedQuadToInteger(prefix2)
+            int_prefix_top = (~ip_utils.makeIntegerMask(
+                cidr2)) | ip_utils.decimalDottedQuadToInteger(prefix2)
             if int_prefix_top - 2 * 32 == -4117887025:
                 print("Test singed to unsigned conversion")
                 # ToDo breakpoint setzen, Werte die die for Schleife ausspuckt mit den erwarteten Ergebnisse zu vergleichen
@@ -456,12 +327,12 @@ def onlyinold_to_sql():
                 #   if int_prefix_top < 0:
                 #      int_prefix_top = int_prefix_top + (2**32)
 
-            prefix_top = integerToDecimalDottedQuad(int_prefix_top)
+            prefix_top = ip_utils.integerToDecimalDottedQuad(int_prefix_top)
             # print("netw.adrr.:{}".format(base))
-            for j in range(decimalDottedQuadToInteger(base) + 1,
-                           decimalDottedQuadToInteger(
-                               integerToDecimalDottedQuad(int_prefix_top)) + 1):
-                list_unpacked_ips.append(integerToDecimalDottedQuad(j))
+            for j in range(ip_utils.decimalDottedQuadToInteger(base) + 1,
+                           ip_utils.decimalDottedQuadToInteger(
+                               ip_utils.integerToDecimalDottedQuad(int_prefix_top)) + 1):
+                list_unpacked_ips.append(ip_utils.integerToDecimalDottedQuad(j))
 
     df = pandas.DataFrame(list_unpacked_ips)
     sqlEngine = create_engine(
