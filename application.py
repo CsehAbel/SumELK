a225034a506cd23c95b9d83a2d245f7d218d8786
abel.kecse <abel.kecse@4p-service.de>
2022-05-25 14:37:38 +0200

    commit instead of stash

--- application.py
+++ application.py
@@ -29,20 +29,18 @@ def main():
     # downloads SecureTrack
     # where rule_name like a.* and like wuser.*  and not like atos_vuln_scan
     # to CSV_DB -> st_ports
-    # systems_group.dest_ports_to_file()
     # first run SGRE to unpack se_ruleset
-    # filepath_qc = "se_ruleset_unpacked22Mar2022.xlsx"
     #filepath_qc = get_cli_args().qualitycheck
     #qc_to_sql.main(filepath_qc)
     #downloading securetrack dest_ip,port
-    #systems_group.dest_ports_to_file()
+    systems_group.dest_ports_to_file()
 
     # download hits to hits/...json
     #hits.main()
     # .json to mysql table 'ip'
-    path = "/mnt/c/Users/z004a6nh/PycharmProjects/SumELK/hits/"
-    regex = "^hit.*"
-    bulk_json_to_df.main(path,regex)
+    #path = "/mnt/c/Users/z004a6nh/PycharmProjects/SumELK/hits/"
+    #regex = "^hit.*"
+    #bulk_json_to_df.main(path,regex)
 
     #systems_group.save_new_transform_json()
 
