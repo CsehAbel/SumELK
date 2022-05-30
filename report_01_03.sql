8838e14a9d50e824bf321e2894ca59670f3abde8
abel.kecse <abel.kecse@4p-service.de>
2022-05-30 23:03:27 +0200

    st_ports column rule_order,display_name removed

--- report_01_03.sql
+++ report_01_03.sql
@@ -17,8 +17,6 @@ SET group_concat_max_len=15000;
 #wa left join st_ports
 SELECT * FROM (SELECT * FROM white_apps_se_ruleset_merged_dns2_grouped_by_ip_app_id) as wa LEFT JOIN
 (SELECT st_dest_ip,rule_name,GROUP_CONCAT(DISTINCT(st_port)) as g_st_port,
-GROUP_CONCAT(DISTINCT(st_serv_name)) as g_st_serv_name,
-GROUP_CONCAT(DISTINCT(rule_order)) as g_rule_order,
 GROUP_CONCAT(DISTINCT(rule_number)) as g_rule_number
 FROM st_ports GROUP BY st_dest_ip,rule_name)
 as ports ON wa.IPs = ports.st_dest_ip WHERE ports.st_dest_ip IS NULL LIMIT 30000;
@@ -27,8 +25,6 @@ as ports ON wa.IPs = ports.st_dest_ip WHERE ports.st_dest_ip IS NULL LIMIT 30000
 SELECT ports.* FROM (SELECT * FROM white_apps_se_ruleset_merged_dns2_grouped_by_ip_app_id 
 ) as wa RIGHT JOIN
 (SELECT st_dest_ip,rule_name,GROUP_CONCAT(DISTINCT(st_port)) as g_st_port,
-GROUP_CONCAT(DISTINCT(st_serv_name)) as g_st_serv_name,
-GROUP_CONCAT(DISTINCT(rule_order)) as g_rule_order,
 GROUP_CONCAT(DISTINCT(rule_number)) as g_rule_number
 FROM st_ports GROUP BY st_dest_ip,rule_name)
 as ports ON wa.IPs = ports.st_dest_ip WHERE wa.ips IS NULL LIMIT 30000;
