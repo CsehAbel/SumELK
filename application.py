78ee53beba4e620d830855d156ca6549f2e44308
abel.kecse <abel.kecse@4p-service.de>
2022-05-31 00:09:46 +0200

    new_transform.json updated

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
 import json
 
+import generate_queries
+import import_rules
 import resolveIpToName
 import systems_group
 import qc_to_sql
@@ -30,20 +32,26 @@ def main():
     # where rule_name like a.* and like wuser.*  and not like atos_vuln_scan
     # to CSV_DB -> st_ports
     # first run SGRE to unpack se_ruleset
-    #filepath_qc = get_cli_args().qualitycheck
-    #qc_to_sql.main(filepath_qc)
-    #downloading securetrack dest_ip,port
-    systems_group.dest_ports_to_file()
+    # filepath_qc = get_cli_args().qualitycheck
+    # qc_to_sql.main(filepath_qc)
+    # path = "./Network-CST-P-SAG-Energy.json"
+    # import_rules.main(path)
 
     # download hits to hits/...json
     #hits.main()
     # .json to mysql table 'ip'
-    #path = "/mnt/c/Users/z004a6nh/PycharmProjects/SumELK/hits/"
-    #regex = "^hit.*"
-    #bulk_json_to_df.main(path,regex)
+    # path = "/mnt/c/Users/z004a6nh/PycharmProjects/SumELK/hits/"
+    # regex = "^hit.*"
+    # bulk_json_to_df.main(path,regex)
 
     #systems_group.save_new_transform_json()
+    onlyinnew = generate_queries.read_query1to4()
+    #new_transform.json
+    generate_queries.save_new_transform_json(onlyInNew=onlyinnew)
+    #upload all_red_networks systems to mysql systems table
+    generate_queries.main()
 
+    # save_new_transform_json(onlyInNew=onlyinnew)
     # resolving ip to fqdn for white_apps
     # each time the ip-fqdn pair will be appended to CSV_DB->src_dns
     # resolveIpToName.resolve_white_apps()
