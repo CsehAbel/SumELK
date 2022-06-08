06fbee22e97ca178c40c170b1c479a92d7dc7ab8
abel.kecse <abel.kecse@4p-service.de>
2022-06-08 03:03:30 +0200

    updated eagle query to use ip field instead of '0' field

--- report_01_03_without_eagle.sql
+++ report_01_03_without_eagle.sql
@@ -5,7 +5,7 @@ WHERE cardinality!=1;
 #222
 SELECT COUNT(*) FROM white_apps_se_ruleset_merged_dns2 WHERE dns4 IS NULL;
 SELECT * FROM white_apps_se_ruleset_merged_dns2 
-LEFT JOIN (SELECT `0` as dip FROM eagle) as e
+LEFT JOIN (SELECT ip as dip FROM eagle) as e
 ON white_apps_se_ruleset_merged_dns2.ips=e.dip
 WHERE dns4 AND (e.dip IS NULL) IS NULL LIMIT 10000;
 
@@ -18,25 +18,25 @@ WHERE dns4 AND (e.dip IS NULL) IS NULL LIMIT 10000;
 SET group_concat_max_len=15000;
 
 #wa left join st_ports
-SELECT * FROM (SELECT * FROM white_apps_se_ruleset_merged_dns2_grouped_by_ip_app_id) as wa LEFT JOIN
+SELECT * FROM (SELECT * FROM white_apps_se_ruleset_merged_dns2_grouped_by_ip_app_id) as wa 
+LEFT JOIN
 (SELECT st_dest_ip,rule_name,GROUP_CONCAT(DISTINCT(st_port)) as g_st_port,
 GROUP_CONCAT(DISTINCT(rule_number)) as g_rule_number
 FROM st_ports GROUP BY st_dest_ip,rule_name) 
 as ports
 ON wa.IPs = ports.st_dest_ip
-LEFT JOIN (SELECT `0` as dip FROM eagle) as e
+LEFT JOIN (SELECT ip as dip FROM eagle) as e
 ON wa.ips=e.dip 
 WHERE (ports.st_dest_ip IS NULL) AND (e.dip IS NULL) LIMIT 30000;
 
 #white_apps unique ip app_id RIGHT JOIN st_ports unique ip, rule_name 
-SELECT ports.* FROM (SELECT * FROM white_apps_se_ruleset_merged_dns2_grouped_by_ip_app_id 
-) as wa 
-RIGHT JOIN
-(SELECT st_dest_ip,rule_name,GROUP_CONCAT(DISTINCT(st_port)) as g_st_port,
+SELECT ports.*,wa.*,e.* FROM (SELECT st_dest_ip,rule_name,GROUP_CONCAT(DISTINCT(st_port)) as g_st_port,
 GROUP_CONCAT(DISTINCT(rule_number)) as g_rule_number
-FROM st_ports GROUP BY st_dest_ip,rule_name)
-as ports 
+FROM st_ports GROUP BY st_dest_ip,rule_name) as ports 
+LEFT JOIN
+(SELECT * FROM white_apps_se_ruleset_merged_dns2_grouped_by_ip_app_id 
+) as wa 
 ON wa.ips = ports.st_dest_ip
-LEFT JOIN (SELECT `0` as dip FROM eagle) as e
+LEFT JOIN (SELECT ip as dip FROM eagle) as e
 ON ports.st_dest_ip=e.dip
 WHERE (wa.ips IS NULL) AND (e.dip IS NULL) LIMIT 30000;
