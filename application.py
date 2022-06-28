9f1b7d164429ca119c5892ceb4b600021d8955d6
abel.kecse <abel.kecse@4p-service.de>
2022-06-28 10:55:55 +0200

    uncommented application.py

--- application.py
+++ application.py
@@ -33,31 +33,30 @@ def main():
     # where rule_name like a.* and like wuser.*  and not like atos_vuln_scan
     # to CSV_DB -> st_ports
     # first run SGRE to unpack se_ruleset
-    # filepath_qc = get_cli_args().qualitycheck
-    # qc_to_sql.main(filepath_qc)
-    # path = "./Network-CST-P-SAG-Energy.json"
-    # import_rules.main(path)
+    filepath_qc = get_cli_args().qualitycheck
+    qc_to_sql.main(filepath_qc)
+    path = "./Network-CST-P-SAG-Energy.json"
+    import_rules.main(path)
 
     #fill mysql tables eagle, snic_export, run eagle_comparison.sql
-    filepath_qc = "20220614-snic_ip_network_assignments.csv"
+    filepath_qc = "20220627-snic_ip_network_assignments.csv"
     eagle_filter.main(filepath_qc)
     eagle_filter.snic_to_sql(filepath_qc)
 
     # download hits to hits/...json
-    #hits.main()
+    hits.main()
     # .json to mysql table 'ip'
-    # path = "/mnt/c/Users/z004a6nh/PycharmProjects/SumELK/hits/"
-    # regex = "^hit.*"
-    # bulk_json_to_df.main(path,regex)
+    path = "/mnt/c/Users/z004a6nh/PycharmProjects/SumELK/hits/"
+    regex = "^hit.*"
+    bulk_json_to_df.main(path,regex)
 
-    #systems_group.save_new_transform_json()
-    #onlyinnew = generate_queries.read_query1to4()
+
+    onlyinnew = generate_queries.read_query1to4()
     #new_transform.json
-    #generate_queries.save_new_transform_json(onlyInNew=onlyinnew)
+    generate_queries.save_new_transform_json(onlyInNew=onlyinnew)
     #upload all_red_networks systems to mysql systems table
-    #generate_queries.main()
+    generate_queries.main()
 
-    # save_new_transform_json(onlyInNew=onlyinnew)
     # resolving ip to fqdn for white_apps
     # each time the ip-fqdn pair will be appended to CSV_DB->src_dns
     # resolveIpToName.resolve_white_apps()
