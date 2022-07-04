778a34c6cf4c0e0a14f193d28e99691d3767f8d1
abel.kecse <abel.kecse@4p-service.de>
2022-07-04 15:35:41 +0200

    application.py eagle_filter.py imported, generate_queries.py old_darwin_transform.json burned in

--- application.py
+++ application.py
@@ -9,6 +9,8 @@ import secrets
 import main as hits
 import bulk_json_to_df
 from sqlalchemy import create_engine
+import darwin_resolve
+import eagle_filter
 
 import secrets
 import pandas
@@ -31,17 +33,17 @@ def main():
     # where rule_name like a.* and like wuser.*  and not like atos_vuln_scan
     # to CSV_DB -> st_ports
     # first run SGRE to unpack se_ruleset
-    #filepath_qc = get_cli_args().qualitycheck
-    #qc_to_sql.main(filepath_qc)
-    #path = "./Network-CST-P-SAG-Darwin.json"
-    #import_rules.main(path)
+    filepath_qc = get_cli_args().qualitycheck
+    qc_to_sql.main(filepath_qc)
+    path = "./Network-CST-P-SAG-Darwin.json"
+    import_rules.main(path)
 
     # download hits to hits/...json
-    # hits.main()
+    hits.main()
     # .json to mysql table 'ip'
-    # path = "/mnt/c/Users/z004a6nh/PycharmProjects/SumELK/darwin_hits/"
-    # regex = "^hit.*"
-    # bulk_json_to_df.main(path,regex)
+    path = "/mnt/c/Users/z004a6nh/PycharmProjects/SumELK/darwin_hits/"
+    regex = "^hit.*"
+    bulk_json_to_df.main(path,regex)
 
     #onlyinnew = generate_queries.read_query1to4()
     darwin_json = "Standard_objects_darwin.json"
@@ -53,10 +55,12 @@ def main():
     #generate_queries.main()
     generate_queries.systems_to_sql(sag_systems)
 
-    # save_new_transform_json(onlyInNew=onlyinnew)
+    snic_path = "20220630-snic_ip_network_assignments.csv"
+    eagle_filter.fill_eagle_from_snic(filepath_qc=snic_path)
+
     # resolving ip to fqdn for white_apps
-    # each time the ip-fqdn pair will be appended to CSV_DB->src_dns
-    # resolveIpToName.resolve_white_apps()
+    # each time the ip-fqdn pair will be appended to DARWIN_DB->src_dns
+    darwin_resolve.resolve_white_apps()
 
 if __name__ == "__main__":
     main()
