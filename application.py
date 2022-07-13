841c3975d080cbf80e86e090b7166b12be7f1ded
abel.kecse <abel.kecse@4p-service.de>
2022-07-13 13:11:07 +0200

    application.py corrected index for acces element of list filled by snic.csv files

--- application.py
+++ application.py
@@ -33,9 +33,10 @@ def get_cli_args():
     return args
 
 def main():
-    # first run SGRE to unpack se_ruleset
-    pttrn_rlst = re.compile("^.+se_ruleset.+\.xlsx$")
-    newest_rlst = file_operations.search_newest_in_folder(Path("./"), pttrn_rlst)
+    # first     run file_operations.py
+    # second    run SGRE to unpack se_ruleset, copy here
+    ptrn = re.compile("se_ruleset_unpacked\d{2}[A-Za-z]{3}\d{4}\.xlsx$")
+    newest_rlst = file_operations.search_newest_in_folder(Path("./"), ptrn)
     print("Using " + newest_rlst.resolve().__str__())
     filepath_qc = newest_rlst.resolve().__str__()
     qc_to_sql.main(filepath_qc)
@@ -49,11 +50,16 @@ def main():
     file_operations.one_file_found_in_folder(filepath_list=filepath_list,
                                              project_dir=Path("./"),
                                              pttrn_snic=re.compile("\d{4}\d{2}\d{2}-snic_ip_network_assignments.csv"))
-    print("%s used to fill mysql tables eagle, snic_export" %filepath_list[1])
+    print("%s used to fill mysql tables eagle, snic_export" %filepath_list[0])
 
     #fill mysql tables eagle, snic_export, run eagle_comparison.sql
-    eagle_filter.main(filepath_list[1])
-    eagle_filter.snic_to_sql(filepath_list[1])
+    eagle_filter.main(filepath_list[0])
+    eagle_filter.snic_to_sql(filepath_list[0])
+
+    # new_transform.json
+    sag_systems = systems_group.get_systems_ip_list(darwin_json=standard_path)
+    generate_queries.save_new_transform_json(onlyInNew=sag_systems)
+    generate_queries.systems_to_sql(sag_systems)
 
     # download hits to hits/...json
     hits.main()
@@ -61,11 +67,6 @@ def main():
     path = "/mnt/c/Users/z004a6nh/PycharmProjects/SumELK/hits/"
     regex = "^hit.*"
     bulk_json_to_df.main(path,regex)
-
-    # new_transform.json
-    sag_systems = systems_group.get_systems_ip_list(darwin_json=standard_path)
-    generate_queries.save_new_transform_json(onlyInNew=sag_systems)
-    generate_queries.systems_to_sql(sag_systems)
 
     # resolving ip to fqdn for white_apps
     # each time the ip-fqdn pair will be appended to CSV_DB->src_dns
